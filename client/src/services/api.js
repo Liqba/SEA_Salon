320403bbbae7ef6f403aa0f0a0ace9ba88ae3282
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2024-07-02 21:23:47 +0700

    add start command

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://seasalon-production.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
